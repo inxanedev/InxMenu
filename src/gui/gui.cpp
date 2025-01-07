f1d56db706fa81e9fcc3fa3518e8dceb80a09112
Remigiusz Schoida <inxaneninja@gmail.com>
2025-01-07 01:26:10 +0100

    Added more features

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -7,9 +7,25 @@ bool infinite_ammo = false;
 bool no_grenade_delay = false;
 bool god_mode = false;
 
+float
+base_speed,
+base_dash_cooldown,
+dash_speed,
+dash_height,
+melee_range,
+melee_damage,
+melee_knockback_strength;
+
 void GUI::InitializeOffsets()
 {
 	god_mode = Offsets::m_god_mode->m_original;
+	base_speed = Offsets::m_base_speed->m_original;
+	base_dash_cooldown = Offsets::m_base_dash_cooldown->m_original;
+	dash_speed = Offsets::m_dash_speed->m_original;
+	dash_height = Offsets::m_dash_height->m_original;
+	melee_range = Offsets::m_melee_range->m_original;
+	melee_damage = Offsets::m_melee_damage->m_original;
+	melee_knockback_strength = Offsets::m_melee_knockback_strength->m_original;
 }
 
 void GUI::Render()
@@ -24,6 +40,27 @@ void GUI::Render()
 		if (ImGui::BeginTabItem("Player")) {
 			if (ImGui::Checkbox("God Mode", &god_mode)) {
 				Offsets::m_god_mode->SetValue(god_mode);
+			}
+			if (ImGui::InputFloat("Base Speed", &base_speed, 0.0f, 1000.0f)) {
+				Offsets::m_base_speed->SetValue(base_speed);
+			}
+			if (ImGui::InputFloat("Base Dash Cooldown", &base_dash_cooldown)) {
+				Offsets::m_base_dash_cooldown->SetValue(base_dash_cooldown);
+			}
+			if (ImGui::InputFloat("Dash Speed", &dash_speed)) {
+				Offsets::m_dash_speed->SetValue(dash_speed);
+			}
+			if (ImGui::InputFloat("Dash Height", &dash_height)) {
+				Offsets::m_dash_height->SetValue(dash_height);
+			}
+			if (ImGui::InputFloat("Melee Range", &melee_range)) {
+				Offsets::m_melee_range->SetValue(melee_range);
+			}
+			if (ImGui::InputFloat("Melee Damage", &melee_damage)) {
+				Offsets::m_melee_damage->SetValue(melee_damage);
+			}
+			if (ImGui::InputFloat("Melee Knockback Strength", &melee_knockback_strength)) {
+				Offsets::m_melee_knockback_strength->SetValue(melee_knockback_strength);
 			}
 			ImGui::EndTabItem();
 		}
--- src/offsets/Offsets.h
+++ src/offsets/Offsets.h
@@ -11,11 +11,26 @@ class Offsets {
 public:
 
 	inline static Offset<bool>* m_god_mode = nullptr;
+	inline static Offset<float>* m_base_speed = nullptr;
+	inline static Offset<float>* m_base_dash_cooldown = nullptr;
+	inline static Offset<float>* m_dash_speed = nullptr;
+	inline static Offset<float>* m_dash_height = nullptr;
+	inline static Offset<float>* m_melee_range = nullptr;
+	inline static Offset<float>* m_melee_damage = nullptr;
+	inline static Offset<float>* m_melee_knockback_strength = nullptr;
 
 	static void Initialize() {
 		if (pBase_GameManager == 0) {
 			throw std::runtime_error("pBase_GameManager is null");
 		}
 		m_god_mode = new Offset<bool>(pBase_GameManager, 0, {0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x619});
+		m_base_speed = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x5bc });
+		m_base_dash_cooldown = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x89c });
+		m_dash_speed = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x8c0 });
+		m_dash_height = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x8c4 });
+		m_melee_range = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x9a8 });
+		m_melee_damage = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x9ac });
+		m_melee_knockback_strength = new Offset<float>(pBase_GameManager, 0, { 0xd28, 0x38, 0x0, 0x30, 0x2a0, 0x9b0 });
+		
 	}
 };
